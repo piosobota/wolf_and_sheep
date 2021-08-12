f3c113bbec46e3c1510a1ae4cb47e8f301294ad1
Emil Burdach <Emil.Burdach.Praca@gmail.com>
2021-08-12 15:53:58 +0200

    Welcome task finished, Board class created, WolfAndSheep class created

--- src/main/java/WolfAndSheep.java
+++ src/main/java/WolfAndSheep.java
@@ -1,5 +1,15 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class WolfAndSheep {
+    public static final int  MINIMAL_BOARD_SIZE=4;
     public static void main(String[] args) {
-        
+        int boardSize = -1;
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj w Grze Wilk i Owca !!!!");
+        System.out.println("by Emil Burdach and Piotr Sobota");
+        System.out.println();
+        }
+
     }
-}
